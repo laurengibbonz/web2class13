450506c54cabb90fb31c9ad1fe69cda750a99086
Lauren <laurengibbonz@gmail.com>
2021-04-19 19:48:33 -0400

    react front end view db

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const port = 5000;
 const uri = process.env.ATLAS_URI;
+const cors = require('cors');
+
+app.use(cors());
 
 // was missing this code from class 12
 app.use(express.json());
@@ -21,18 +24,24 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model("Contact", contactSchema);
 
-app.get('/', (req, res) => {
+app.get('/contacts', (req, res) => {
     Contact.find()
         .then(data => res.json(data))
         .catch(err => res.status(400).json('Error' + err));
 });
 
-app.post('/add', (req, res) => {
+app.post('/contacts/add', (req, res) => {
     const newContact = new Contact(req.body);
   
     newContact.save()
       .then(() => res.json('Contact added!'))
       .catch(err => res.status(400).json('Error: ' + err));
+});
+
+app.get('/contacts/delete/:id', (req, res) => {
+    Contact.findByIdAndDelete(req.params.id)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('Error' + err));
 });
 
 app.listen(
